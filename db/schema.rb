6e218d7da2b3297649cb71a3bebf2ccaae08babb
rehmanh <hbieb949@gmail.com>
2018-06-11 16:42:28 -0500

    Added User Model, seedfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611160337) do
+ActiveRecord::Schema.define(version: 20180611195911) do
 
   create_table "procedures", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.text     "complaint"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Procedure.create!([
+  {
+    name: 'The Thing', 
+    description:
+    %{
+      This is a very descriptive description and a lot of content goes here, for some particular reason.
+    }.squish
+  },
+  {
+    name: 'The Flutterby',
+    description:
+    %{
+      The name of this description basically speaks for itself here, there is nothing to see here, other than the fact that there will be a butterfly somehow involved...
+    }.squish
+  }
+])
+
+User.create!([
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone_number: '03009233780',
+    complaint:
+    %{
+      I have a headache and I don't think it is relatd to Dermatlogy, but here I am anyways
+    }.squish
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    phone_number: '666-666-6666',
+    complaint:
+    %{
+      Why won't any one love me in this cule cruel world?   
+    }.squish
+  },
+  {
+    name: 'Charlie Man',
+    email: 'charles@mann.com',
+    phone_number: '03009233780',
+    complaint: 
+    %{
+      I have this urge to always bite people and I am not sure why...
+    }.squish
+  },
+  {
+    name: 'Hanson the Unicorn',
+    email: 'hanson@theunicorn.com',
+    phone_number: '67867867867867867867867867',
+    complaint: 
+    %{
+      Charlie bit me, and it reaaally hurt! :(
+    }.squish
+  }
+
+])
