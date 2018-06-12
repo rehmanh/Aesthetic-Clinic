85cbd6517c5f5849c23b274137dd30387802d8c9
rehmanh <hbieb949@gmail.com>
2018-06-12 15:37:04 -0500

    Added form-level validation to Patient forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,22 @@ class UsersController < ApplicationController
     redirect_to @user
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to @user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
 private
 
   def user_params
