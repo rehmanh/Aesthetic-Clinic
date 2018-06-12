e270f14d49502e58070f47e6d7bc52af5c4f38c8
rehmanh <hbieb949@gmail.com>
2018-06-12 13:32:22 -0500

    Need to fix blank new users / add delete and edit functionality for users

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -8,4 +8,36 @@ class ProceduresController < ApplicationController
     @procedure = Procedure.find(params[:id])
   end
 
+  def edit
+    @procedure = Procedure.find(params[:id])
+  end
+
+  def update
+    @procedure  = Procedure.find(params[:id])
+    @procedure.update(procedure_params)
+    redirect_to @procedure # rails knows to go to the viewpage here
+  end
+
+  def new
+    @procedure = Procedure.new
+  end
+
+  def create
+    @procedure = Procedure.new(procedure_params)
+    @procedure.save
+    redirect_to @procedure
+  end
+
+  def destroy
+    @procedure = Procedure.find(params[:id])
+    @procedure.destroy
+    redirect_to @procedure
+  end
+
+private
+  
+  def procedure_params
+    procedure_params = params.require(:procedure).permit!
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+private
+
+  def user_params
+    user_params = params.require(:user).permit!
+  end
+
 end
