c71155b48dfabe26641e2ef227d754080d9b4ee8
rehmanh <hbieb949@gmail.com>
2018-06-14 16:21:37 -0500

    Changes to User model, phone number validation needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+\.\w+/ , message: "Please enter a valid email address" }  
+  validate :validate_phone_number
+
+private
+
+  def validate_phone_number
+    if phone_number !~ /\d{11}/
+      flash[:notice] = "Please enter a valid phone number"
+    end
+  end
+
 end
