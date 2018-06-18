1e614b989607287b58fac02ee07a8b990f6e935e
rehmanh <hbieb949@gmail.com>
2018-06-18 16:24:55 -0500

    Need to add require sign in validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,21 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:alert] = "Incorrect Email and/or Password. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Sign out successful."
+    redirect_to procedures_url
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  before_action :require_signin, except: [:new, :create]
   
   def index
     @users = User.all
@@ -15,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Your account was successfully created."
       redirect_to @user
     else
@@ -25,6 +28,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to @user
     flash[:notice] = "Your account was successfully deleted."
   end
