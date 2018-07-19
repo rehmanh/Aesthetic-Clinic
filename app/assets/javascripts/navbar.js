b018a19e0f799641db4a481c87d0d22bf4fc95c2
rehmanh <hbieb949@gmail.com>
2018-07-18 22:57:29 -0500

    Need to perform sidebar cleanup

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,11 +1,34 @@
 $(document).on('turbolinks:load', function() {
+  
+ //  var num_clicks = 0;
+
+ //  $("#menuHamburger").click(function() {
+ //    $(this).toggleClass("is-active");
+ //    if (num_clicks % 2 == 0) {
+ //      $(this).animate({
+ //        marginLeft: '20%'
+ //      });
+ //    } else {
+ //      $(this).animate({
+ //        marginLeft: '0'
+ //      });
+ //    }
+ //    $("#sideBar").toggleClass("minimized");  
+ //    ++num_clicks;
+ //  });
 
   $("#menuHamburger").click(function() {
     $(this).toggleClass("is-active");
-    $(this).animate({
-      marginLeft: '25%'
-    });
-    $("#sideBar").toggleClass("minimized");  
+    $(this).css('visibility', 'hidden');
+    $("#sideHamburger").toggleClass("is-active");
+    $("#sideBar").toggleClass("minimized");
+  });
+
+  $("#sideHamburger").click(function() {  
+    $(this).toggleClass("is-active");
+    $("#menuHamburger").css('visibility', 'visible');
+    $("#menuHamburger").toggleClass("is-active");
+    $("#sideBar").toggleClass("minimized");
   });
 
 });
